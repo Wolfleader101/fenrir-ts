d59f249fba55d2d7d64b5e7e6b1828f626ef06dc
Wolfeader101 <james@themorgans.com.au>
2026-01-03 20:12:17 +0800

    feat: default components

---
+++ src/core/ECS/DefaultComponents.ts
@@ -0,0 +1,55 @@
+import { Vector3, Quaternion } from "three";
+import { defineComponent } from "./Component";
+import type { Entity, EntityList } from "./EntityList";
+
+export type Transform = {
+  position: Vector3;
+  rotation: Quaternion;
+  scale: Vector3;
+};
+
+export const Transform = defineComponent<Transform>("Transform");
+
+export type WorldTransform = {
+  position: Vector3;
+  rotation: Quaternion;
+  scale: Vector3;
+};
+
+export const WorldTransform = defineComponent<WorldTransform>("WorldTransform");
+
+export type Name = {
+  name: string;
+};
+
+export const Name = defineComponent<Name>("Name");
+
+export type Relationship = {
+  parent: Entity;
+  firstChild: Entity;
+  nextSibling: Entity;
+  prevSibling: Entity;
+};
+
+export const Relationship = defineComponent<Relationship>("Relationship");
+
+// Helper creator so you never allocate new objects accidentally elsewhere
+export function makeRelationship(nullEntity: Entity): Relationship {
+  return {
+    parent: nullEntity,
+    firstChild: nullEntity,
+    nextSibling: nullEntity,
+    prevSibling: nullEntity,
+  };
+}
+
+export function applyDefaultComponents(entities: EntityList, e: Entity) {
+  entities.set(e, Transform, {
+    position: new Vector3(),
+    rotation: new Quaternion(),
+    scale: new Vector3(1, 1, 1),
+  });
+
+  entities.set(e, Name, { name: `Entity ${entities.idOf(e)}` }); // or unpackId(e)
+  entities.set(e, Relationship, makeRelationship(entities.nullEntity()));
+}
