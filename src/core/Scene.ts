f1cef96ba32ad66f31778a50f8e3c304e42de8fe
Wolfeader101 <james@themorgans.com.au>
2026-01-06 22:48:59 +0800

    feat: scene add skybox

--- src/core/Scene.ts
+++ src/core/Scene.ts
@@ -1,11 +1,34 @@
 import { EntityList } from "./ECS";
+import type { SkyboxDescriptor } from "./Skybox/SkyboxComponents";
 
 export class Scene {
   public name: string;
   public readonly entityList: EntityList;
+  public skybox?: SkyboxDescriptor;
 
   constructor(name: string) {
     this.name = name;
     this.entityList = new EntityList();
+  }
+
+  /**
+   * Set the skybox for this scene
+   */
+  setSkybox(skybox: SkyboxDescriptor | undefined): void {
+    this.skybox = skybox;
+  }
+
+  /**
+   * Remove the skybox from this scene
+   */
+  removeSkybox(): void {
+    this.skybox = undefined;
+  }
+
+  /**
+   * Check if this scene has a skybox
+   */
+  hasSkybox(): boolean {
+    return this.skybox !== undefined && (this.skybox.enabled ?? true);
   }
 }
